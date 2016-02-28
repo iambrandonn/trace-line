d5abbd72a71691f439c171915f2ccecc8364d26b
Brandon Nicholls <brandon.nicholls@gmail.com>
2016-02-27 22:29:34 -0700

    Fix differing ways of specifying filenames

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function(value) {
   var trace = new Error().stack;
-  var results = /.*\n.*\n.*at (.*?):(\d*)/.exec(trace);
-  var message = '--- Line #' + results[2] + ' of ' + results[1] + ' executed';
+  var results = /Error\n.*\n.*at (.*?\()?(.*?):(\d*)/.exec(trace);
+  var message = '--- Line #' + results[3] + ' of ' + results[2] + ' executed';
   if (value === undefined) {
     console.log(message);
     return;
