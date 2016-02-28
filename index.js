1a5270584d24a90e64f9164bdd0b955daefd2371
Brandon Nicholls <brandon.nicholls@gmail.com>
2016-02-27 21:36:14 -0700

    Allow for better printling of objects.  Wording changes.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = function(value) {
   var trace = new Error().stack;
   var results = /.*\n.*\n.*at (.*?):(\d*)/.exec(trace);
-  var message = '--- Line #' + results[2] + ' of ' + results[1] + ' hit';
+  var message = '--- Line #' + results[2] + ' of ' + results[1] + ' executed';
   if (value === undefined) {
     console.log(message);
     return;
   }
-  console.log(message + ' with value of:');
-  console.log('\t' + value + '\n');
+  console.log(message + ' with a value of:');
+  console.log(value);
 }
