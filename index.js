35d66f13a212fbed7f78e4f6f14c6f5bac16563b
Brandon Nicholls <brandon.nicholls@gmail.com>
2016-03-03 11:31:14 -0700

    Added ability to use a globally defined variable

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-module.exports = function(value) {
+function trace(value) {
   var trace = new Error().stack;
   var results = /Error\n.*\n.*at (.*?\()?(.*?):(\d*)/.exec(trace);
   var message = '--- Line #' + results[3] + ' of ' + results[2] + ' executed';
-  if (value === undefined) {
+  if (arguments.length === 0) {
     console.log(message);
     return;
   }
   console.log(message + ' with a value of:');
   console.log(value);
+}
+
+module.exports = trace;
+if (global !== undefined) {
+  global.trace = trace
 }
